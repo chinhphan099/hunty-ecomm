f95a5bbed0fd8c4d38b528c32b09f0dad3d0927b
Chinh Phan <chinh.phan@dfo.global>
2020-04-22 10:37:37 +0700

    style guide

--- app/js/site.js
+++ app/js/site.js
@@ -7,7 +7,18 @@
 (function(global, undefined) {
   'use strict';
 
-  function scrollTopAfterCollapse() {}
+  function scrollTopAfterCollapse(elmScroll, handle, isPos) {
+    var spaceToTop = 0,
+      offsetHandle = isPos ? handle.position().top : handle.offset().top;
+
+    if(!isPos) {
+      spaceToTop = 50;
+    }
+
+    elmScroll.stop().animate({
+      scrollTop: offsetHandle - spaceToTop
+    }, 400);
+  }
 
   global.Site = {
     scrollTopAfterCollapse: scrollTopAfterCollapse
