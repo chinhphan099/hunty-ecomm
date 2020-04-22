45a7b97ed1bbfc16de3ccac033045b3548ef72a8
Chinh Phan <chinhphan099@gmail.com>
2020-04-22 23:04:44 +0700

    custom select

--- app/js/guide/components/select.js
+++ app/js/guide/components/select.js
@@ -1,106 +1,33 @@
-/**
- *  @name select
- *  @version 1.0
- *  @options
- *    option
- *  @events
- *    event
- *  @methods
- *    init
- *    updateValue
- *    destroy
- */
-
-;(function($, window, undefined) {
-  'use strict';
-
-  var pluginName = 'select';
-
-  function Plugin(element, options) {
-    this.element = $(element);
-    this.options = $.extend({}, $.fn[pluginName].defaults, this.element.data(), options);
-    this.init();
-  }
-
-  Plugin.prototype = {
-    init: function() {
-      var that = this;
-        this.valueContainer = this.element.find(this.options.valueContainer);
-        this.selectElm = this.element.find(this.options.selectElm);
-
-      this.updateValue();
-      this.selectElm.off('change.changeValue').on('change.changeValue', function() {
-        var idSelectbox = $(this).attr('id');
-        that.updateValue();
-        if($(this).prop('required') && $(this).is('[aria-describedby]')) {
-          $(this).closest('form').validate().element('#' + idSelectbox);
-        }
-      });
-    },
-    updateValue: function() {
-      var getValue = this.selectElm.find('option:selected').text();
-
-      this.valueContainer.text(getValue);
-      if(this.selectElm.val() !== '') {
-        this.element.addClass(this.options.selectedCls);
-      }
-      else {
-        this.element.removeClass(this.options.selectedCls);
-      }
-    },
-    destroy: function() {
-      this.fileInput.off('click.' + pluginName);
-      $.removeData(this.element[0], pluginName);
-    }
-  };
-
-  $.fn[pluginName] = function(options, params) {
-    return this.each(function() {
-      var instance = $.data(this, pluginName);
-      if (!instance) {
-        $.data(this, pluginName, new Plugin(this, options));
-      } else if (instance[options]) {
-        instance[options](params);
-      }
-    });
-  };
-
-  $.fn[pluginName].defaults = {
-    valueContainer: 'span',
-    selectElm: 'select',
-    selectedCls: 'selected'
-  };
-
-  $(function() {
-    $('[data-' + pluginName + ']')[pluginName]();
-  });
-}(jQuery, window));
-
-/*
 class CustomSelect {
   constructor(elm) {
-    this.elements = document.querySelectorAll(elm);
-    this.selects = this.elements.querySelector('select');
+    this.selects = document.querySelectorAll(elm);
     this.selectedCls = 'selected';
+    this.initCls = 'select-inited';
   }
 
-  listener() {
-    Array.prototype.slice.call(this.selects).forEach(select => {
-      select.addEventListener('change', (e) => {
-        if(e.currentTarget.value !== '') {
-          e.currentTarget.classList.add(this.selectedCls);
-        }
-        else {
-          e.currentTarget.classList.remove(this.selectedCls);
-        }
-      });
-    });
+  checkSelectedValue(select) {
+    if(select.value !== '') {
+      select.parentNode.classList.add(this.selectedCls);
+    }
+    else {
+      select.parentNode.classList.remove(this.selectedCls);
+    }
   }
 
   init() {
-    this.listener();
+    Array.prototype.slice.call(this.selects).forEach(select => {
+      this.checkSelectedValue(select);
+
+      // If select was init before(has class select-inited) then return.
+      if(select.parentNode.classList.contains(this.initCls)) {return;}
+
+      select.parentNode.classList.add(this.initCls);
+      select.addEventListener('change', (e) => {
+        this.checkSelectedValue(e.currentTarget);
+      });
+    });
   }
 }
 
-new CustomSelect('[data-select]').init();
-*/
+const customSelect = new CustomSelect('[data-select] select');
+customSelect.init();
