2e0ed2132beaa98d7b3e8c723a0de53552d7df5f
Chinh Phan <chinhphan099@gmail.com>
2020-04-28 23:32:18 +0700

    manage shop

--- app/js/site.js
+++ app/js/site.js
@@ -22,7 +22,7 @@
 
   function searchIndex() {
     const searchBtn = document.querySelector('.header__search-btn');
-    if(searchBtn) {
+    if(!!searchBtn) {
       searchBtn.addEventListener('click', () => {
         document.querySelector('body').classList.add('open-search');
       });
@@ -36,8 +36,45 @@
     }
   }
 
+  function loginModal() {
+    const loginBtn = document.querySelector('.header__login');
+    if(!!loginBtn) {
+      loginBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector('body').classList.add('open-modal');
+      });
+    }
+
+    const closeModal = document.querySelector('.modal .close');
+    if(!!closeModal) {
+      closeModal.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector('body').classList.remove('open-modal');
+      });
+    }
+  }
+
+  function handleTabEvents() {
+    const tabItems = document.querySelectorAll('[data-collapse] [data-handle]');
+    Array.prototype.slice.call(tabItems).forEach(tabItem => {
+      tabItem.addEventListener('click', (e) => {
+        const dataContent = e.currentTarget.dataset.handle;
+        const wrapper = e.currentTarget.closest('[data-collapse]');
+        const contentItem = wrapper.querySelector(`[data-content="${dataContent}"]`);
+        if(!contentItem.classList.contains('active')) {
+          wrapper.querySelector('[data-content].active').classList.remove('active');
+          wrapper.querySelector('[data-active].active').classList.remove('active');
+          e.currentTarget.closest('[data-active]').classList.add('active');
+          contentItem.classList.add('active');
+        }
+      });
+    });
+  }
+
   function listener() {
     searchIndex();
+    loginModal();
+    handleTabEvents();
   }
 
   function init() {}
